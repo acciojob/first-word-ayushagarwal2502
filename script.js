263bb6d41976abcdbb82ed99349cc93e493911b0
ayushagarwal2502 <159728868+ayushagarwal2502@users.noreply.github.com>
2025-01-10 02:13:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function firstWord(s) {
   // your code here
+	let a='';
+	for(let i=0;i<s.length;i++){
+		if(s[i]==' ' && i!=0){
+			return a;
+		}
+		else{
+			a+=s[i];
+		}
+	}
+	return a;
 }
 
 // Do not change the code below
-
+   
 const s = prompt("Enter String:");
 alert(firstWord(s));
